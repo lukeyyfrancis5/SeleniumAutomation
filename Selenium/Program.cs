8f63ab2bde9862239aa067acb4e08442116c4ac7
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-14 22:17:22 -0400

    - Selenium Work

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Selenium
 {
     class Program
     {
+        // Create the reference for Browser
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
+           
+        }
+
+
+        [SetUp]
+        public void Intialize()
+        {
+            // Navigate to Google page
+            driver.Navigate().GoToUrl("executeautomation.com/demosite/index.html?UserName=&Password=&Login=Login");
+            Console.WriteLine("Open URL");
+        }
+
+
+        [Test]
+        public void ExecuteTest()
+        {
+
+            // Enter Text(element, value, type)
+
+
+
+            // Find the Element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            // Preform Ops
+            element.SendKeys("execute automation");
+            Console.WriteLine("Execute Test");
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("Next Method");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            // Quit
+            driver.Close();
+            Console.WriteLine("Close Browser");
+
         }
     }
 }
